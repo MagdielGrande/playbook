7706101bcacc8d9ac81567ad9c1ee9358e7a8922
Magdiel Hermosillo Grande <magdielhgra@gmail.com>
2022-04-19 15:45:19 -0500

    Creacion de una clase y su modularizacion III

---
+++ weekly_mission_3/examples/my_launch_app/index.js
@@ -0,0 +1,8 @@
+const MissionCommander = require('./app/missionCommander');
+const carlo = new MissionCommander("Carlo")
+const magdiel = new MissionCommander("Magdiel")
+const pedro = new MissionCommander("Pedro")
+
+console.log(carlo.name)
+console.log(magdiel.name)
+console.log(pedro.name)
