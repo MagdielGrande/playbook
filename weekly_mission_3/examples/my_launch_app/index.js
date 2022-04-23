71405d0c7b1f55000a910b18f820659109b94cbe
Magdiel Hermosillo Grande <magdielhgra@gmail.com>
2022-04-23 15:53:41 -0500

    Se movieron los archivos de la semana 3 para ponerlos en repositorios separados

--- weekly_mission_3/examples/my_launch_app/index.js
+++
@@ -1,8 +0,0 @@
-const MissionCommander = require('./app/missionCommander');
-const carlo = new MissionCommander("Carlo")
-const magdiel = new MissionCommander("Magdiel")
-const pedro = new MissionCommander("Pedro")
-
-console.log(carlo.name)
-console.log(magdiel.name)
-console.log(pedro.name)
